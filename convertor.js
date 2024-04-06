5230470e825b7c9b4f3b6fc85024acb0c7690e7e
Anwarul07 <mdanwarul064@gmail.com>
2024-04-06 20:01:10 +0530

    update convertor.js

--- convertor.js
+++ convertor.js
@@ -2,20 +2,30 @@ console.log("Welcome to currency Converting");
 
 
 const populate = async (value, currency) => {
+
+    let myStr = ""
     url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_UuuFEXsnVGwPcIk3Fry6Btp6HMSLWldutE5FA9Wk&base_currency="
 
     let response = await fetch(url);
-    let rjson = await response.json();
-    console.log(rjson)
+    let rJson = await response.json();
+    console.log(rJson)
 
-    const tablebody = document.querySelector("tbody");
-    tablebody.innerHTML = `
-<tr>
-  <td>Data1 </td>
-  <td>Data2</td>
-  <td>Data3</td>
-</tr>`
+    document.querySelector(".output").style.display = "block"
+
+    for (let key of Object.keys(rJson["data"])) {
+        myStr += ` <tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
+                    </tr> `
+    }
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
+
+
+
 }
+
 
 const btn = document.querySelector(".btn");
 btn.addEventListener("click", (e) => {
